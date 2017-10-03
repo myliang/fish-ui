142667deaeb62b41d27ca642cc2073cf6b4aff32
myliang <liangyuliang0335@126.com>
2017-10-03 21:40:18 +0800

    ....

--- examples/components/DemoIndex.vue
+++ examples/components/DemoIndex.vue
@@ -1,15 +1,15 @@
 <template>
     <main-layout menuActiveIndex="index">
       <h2>Fish-UI</h2>
-      <p>这里是 fish 的 fish 实现，开发和服务于后台产品</p>
+      <p>基于 vue2, semantic-ui  实现，开发和服务于后台产品</p>
       <h3>支持环境</h3>
       <p>现代浏览器（暂时主要是chrome）</p>
       <h3>版本</h3>
-      <p>1.0.0.beta</p>
+      <p>1.0.0</p>
       <h3>安装</h3>
       <p>我们推荐使用 npm 的方式进行开发，不仅可在开发环境轻松调试，也可放心地在生产环境打包部署使用，享受整个生态圈和工具链带来的诸多好处。</p>
       <p>如果你的网络环境不佳，推荐使用 cnpm。</p>
-      <pre v-highlightjs><code class="bash">npm install fish --save</code></pre>
+      <pre v-highlightjs><code class="bash">npm install fish-ui --save</code></pre>
       <h3>快速上手</h3>
       <p>推荐是用vue-cli创建项目</p>
       <h3>引用fish,全局引用即可</h3>
