800110a83db25f682ab1f79b48768221ad362b74
myliang <liangyuliang0335@126.com>
2020-01-01 21:42:30 +0800

    table.key support key#key1#key2

--- examples/components/DemoTableColumnType.vue
+++ examples/components/DemoTableColumnType.vue
@@ -11,12 +11,13 @@
           {title: '', type: 'checkbox', width: '50', align: 'center'},
           {title: 'Name', key: 'name'},
           {title: 'age', key: 'age'},
-          {title: 'Address', key: 'address'}
+          {title: 'Address', key: 'address'},
+          {title: 'obj#key', key: 'obj#key'}
         ],
         data: [
-          {name: 'yanbin.hu', age: 32, address: 'haidi part 1, xihu, Hangzhou'},
-          {name: 'yanzu.wu', age: 35, address: 'haidi part 5, xihu, Hangzhou'},
-          {name: 'yanzu.wu', age: 35, address: 'haidi part 5, xihu, Hangzhou'},
+          {name: 'yanbin.hu', age: 32, address: 'haidi part 1, xihu, Hangzhou', obj: { key: 'key1' }},
+          {name: 'yanzu.wu', age: 35, address: 'haidi part 5, xihu, Hangzhou', obj: { key: 'key2' }},
+          {name: 'yanzu.wu', age: 35, address: 'haidi part 5, xihu, Hangzhou', obj: { key: 'key3' }},
           {name: 'yanzu.wu', age: 35, address: 'haidi part 5, xihu, Hangzhou'}
         ]
       }
